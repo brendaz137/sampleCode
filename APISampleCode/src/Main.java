2697e2f7bb9d9165ac56264ed1c83fa2a0f11c45
Bo Jiang <bojiang@mit.edu>
2015-06-24 09:40:29 -0400

    updated java

--- APISampleCode/src/Main.java
+++ APISampleCode/src/Main.java
@@ -3,51 +3,45 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class Main {
     public static void main(String[] args){
-		 // URL to call
-		 String urlString = "http://mit-public-dev.cloudhub.io/classrooms/v1/classrooms"; 
-       HttpURLConnection urlConnection = null;
+		// URL to call
+    	String urlString = "http://mit-public-dev.cloudhub.io/classrooms/v1/classrooms"; 
+    	HttpURLConnection urlConnection;
+    	StringBuffer response = new StringBuffer();
+    	JSONObject jsonObject;
        
-       BufferedReader inputStream;
-       StringBuffer response = new StringBuffer();
-       
-       JSONObject jsonObject;
-       JSONArray jsonArray;
-       
-       
-      
+         
        try {
-           URL url = new URL(urlString);
+    	   URL url = new URL(urlString);
            urlConnection = (HttpURLConnection) url.openConnection();
            
            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-   		 String inputLine;
-   		 
-
-   		 while ((inputLine = in.readLine()) != null) {
-   			response.append(inputLine);
-   		 }
-   		      		 
-   		 in.close();
-   		 
-   		 try {
-        		 jsonObject = new JSONObject(response.toString());
-        		 System.out.println(jsonObject);
-       		 //System.out.println(jsonArray);
-       		 //jsonObject = jsonArray.getJSONObject(0);
-       		 //System.out.println(jsonObject.getString("predictions_url"));
-   		 } catch (JSONException e){
-          	 System.out.println(e.toString());
-   		 }
-       } catch (Exception e) {
+           String inputLine;
+           
+           while ((inputLine = in.readLine()) != null) {
+        	   response.append(inputLine);
+           }
+           
+           in.close();
+           
+           try {
+        	   jsonObject = new JSONObject(response.toString());
+        	   System.out.println(jsonObject);
+        	   //System.out.println(jsonObject.getString("data"));
+        	   } 
+           catch (JSONException e){
+          	   System.out.println(e.toString());
+          	   }
+           } 
+       
+       catch (Exception e) {
            // if any I/O error occurs
-      	 System.out.println(e.toString());
+           System.out.println(e.toString());
        }    
 	}
 }
