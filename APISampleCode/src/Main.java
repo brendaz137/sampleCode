fe600be4088a867ee7c1e0cfa1564a3d9f7cc625
Bo Jiang <bojiang@mit.edu>
2015-06-24 11:29:05 -0400

    2urls

--- APISampleCode/src/Main.java
+++ APISampleCode/src/Main.java
@@ -10,35 +10,61 @@ public class Main {
     public static void main(String[] args){
 		// URL to call
     	String urlString = "http://mit-public-dev.cloudhub.io/classrooms/v1/classrooms/1-115"; 
+    	String urlStringAll = "http://mit-public-dev.cloudhub.io/classrooms/v1/classrooms";
     	HttpURLConnection urlConnection;
     	StringBuffer response = new StringBuffer();
+    	StringBuffer responseAll = new StringBuffer();
     	JSONObject jsonObject;
        
          
-       try {
-    	   URL url = new URL(urlString);
-           urlConnection = (HttpURLConnection) url.openConnection();
+       try {   //this begins the first url
+	    	   URL url = new URL(urlString);
+	           urlConnection = (HttpURLConnection) url.openConnection();
+	           
+	           BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	           String inputLine;
+	           
+	           while ((inputLine = in.readLine()) != null) {
+	        	   response.append(inputLine);
+	           }
+	           
+	           in.close();
+	           
+	           try {
+	        	   jsonObject = new JSONObject(response.toString());
+	        	   System.out.println(jsonObject);
+	        	   System.out.println(jsonObject.getJSONObject("data").getString("building"));
+	           } catch (JSONException e){
+	          	   System.out.println(e.toString());
+	           }
+	           //this ends the first url
+	           
+	           
+	           
+	           //this begins the second url
+	           url = new URL(urlStringAll);
+	           urlConnection = (HttpURLConnection) url.openConnection();
+	           
+	           in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	           
+	           while ((inputLine = in.readLine()) != null) {
+	        	   responseAll.append(inputLine);
+	           }
+	           
+	           in.close();
+	           
+	           try {
+	        	   jsonObject = new JSONObject(responseAll.toString());
+	        	   System.out.println(jsonObject);
+	        	   //System.out.println(jsonObject.getJSONObject("data").getString("building"));
+	           } catch (JSONException e){
+	          	   System.out.println(e.toString());
+	           }
+	           //this ends the second url
+	           
+	           
            
-           BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-           String inputLine;
-           
-           while ((inputLine = in.readLine()) != null) {
-        	   response.append(inputLine);
-           }
-           
-           in.close();
-           
-           try {
-        	   jsonObject = new JSONObject(response.toString());
-        	   System.out.println(jsonObject);
-        	   System.out.println(jsonObject.getJSONObject("data").getString("building"));
-        	   } 
-           catch (JSONException e){
-          	   System.out.println(e.toString());
-          	   }
-           } 
-       
-       catch (Exception e) {
+           } catch (Exception e) {
            // if any I/O error occurs
            System.out.println(e.toString());
        }    
